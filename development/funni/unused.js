59108370e6af0529e7218b69c0c9e7b9c391d895
WlodekM <wlodzio.maly@gmail.com>
2023-02-13 13:44:14 +0200

    HYPERLINK_BLOCKED()

---
+++ development/funni/unused.js
@@ -0,0 +1,10 @@
+var buisness = "unused"                                     //Where... Where am I?
+function HYPERLINK_BLOCKED() {document.write(buisness);}    //Hello...? Anyone...?
+                                                            //Is... anybody out there...?
+                                                            //Someone!?
+                                                            //Anyone!?
+                                                            //Can anyone hear me!?
+                                                            //... It's dark.
+                                                            //It's so dark here.
+                                                            //Someone, anyone, if you can hear me...
+                                                            //Say something... please...
