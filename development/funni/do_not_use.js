e6cd8a2f1b0dd71de2726ce65d94974d6b5d5a95
WlodekM <wlodzio.maly@gmail.com>
2023-02-21 10:07:45 +0200

    a

--- development/funni/do_not_use.js
+++ development/funni/do_not_use.js
@@ -8,17 +8,5 @@
                                     //Huh?
                                     //What no I didn't hear anything just now
                                     //but it sounded like they were talking to you
-function become_a_BIG_SHOT() {
-
-    let nothing = " Hello? can anyone hear me?\n.....\nHuh?\nWhat no I didn\'t hear anything just now\nbut it sounded like they were talking to you"
-    console.log(nothing)
-
-}
-function not() {
-    fetch('egg.txt', {
-    headers: {
-       'Accept': 'application/json'
-    }})
- .then(response => response.text())
- .then(text => document.write(text))
-}
+                                    function become_a_BIG_SHOT(){let nothing=" Hello? can anyone hear me?\n.....\nHuh?\nWhat no I didn't hear anything just now\nbut it sounded like they were talking to you";console.log(nothing)}function not(){fetch("egg.txt",{headers:{Accept:"application/json"}}).then(response=>response.text()).then(text=>document.write(text))}
+                                    
