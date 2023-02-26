3d16601900b170b4895d7f9061966edeced461c8
WlodekM <wlodzio.maly@gmail.com>
2023-02-26 16:54:38 +0200

    98lib re-write

--- 98lib.js
+++ 98lib.js
@@ -1,92 +1,79 @@
+/* add theese for it to work
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+<script src="https://ianrenton.github.io/web3point1/code.js"></script>
+*/
+// New window: show and bring to top
+function newWindow(element) {
+  $(element).show();
+  $(element).selectWindow();
 
-console.log("┍━━━━━━━━━━━━━━━━━━━┑");
-console.log("│ 98LIB.js  started │");
-console.log("│      loading      │");
-console.log("│                   │");
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-      // if present, the header is where you move the DIV from:
-      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      elmnt.onmousedown = dragMouseDown;
-    }
-  
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
-    }
-  
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      // set the element's new position:
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-  
-    function closeDragElement() {
-      // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
+  if (element == '#doom') {
+    loadDoom();
   }
-  console.log("│drag Element loaded│");
-  console.log("│                   │");
+}
 
-  function closewindow(tid){
-    var windowelem = document.getElementById(tid)
-    windowelem.remove()
+
+// Load DOOM, because we can
+function loadDoom() {
+$.getScript('https://js-dos.com/6.22/current/js-dos.js', function() {
+  Dos(document.getElementById("doomcanvas"), {
+        wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+        cycles: 1000,
+        autolock: false,
+    }).ready(function (fs, main) {
+      fs.extract("https://js-dos.com/cdn/upload/DOOM-@evilution.zip").then(function () {
+        main(["-c", "cd DOOM", "-c", "DOOM.EXE"]).then(function (ci) {
+            window.ci = ci;
+        });
+      });
+    });
+});
+}
+
+$( function() {
+  // Window drag
+  $( ".window" ).draggable({ handle: "div.title-bar" });
+
+  // Window resize
+  $( ".window" ).resizable({ handles: "all", alsoresize: ".window-body" });
+
+  // Window close
+  $('.windowclose').on("dblclick", function () { $(this).parents('div.window').hide(); });
+
+  // Window click-to-bring-to-top
+  (function() {
+      var highest = 100;
+
+      $.fn.selectWindow = function() {
+          // Make top
+          this.css('z-index', ++highest);
+          // Make this window selected and others not
+          this.addClass('selectedwindow');
+          $('.window').not(this).each(function(){
+              $(this).removeClass('selectedwindow');
+           });
+      };
+  })();
+  $('.window').mousedown(function() {
+      $(this).selectWindow();
+  });
+
+  // Icon single click
+  $('.icon').click(function() {
+    $(this).find('p').toggleClass('highlight');
+  });
+} );
+// make _ button work
+function foldElement(tid){
+  let btn = document.getElementById(tid+'content');
+  if (btn) {
+      let disabled = btn.hasAttribute('hidden');
+      if (disabled) {
+        btn.removeAttribute("hidden");
+      } else {
+        btn.setAttribute("hidden", "");
+      }
   }
-  console.log("│  Closing  loaded  │");
-  console.log("│                   │");
-  function resizableElement(tid){
-    tid.style.resize="both"
-    tid.style.overflow="auto"
-  }
-  console.log("│   Resize loaded   │");
-  console.log("│                   │");
-  function foldElement(tid){
-    let btn = document.getElementById(tid+'content');
-    if (btn) {
-        let disabled = btn.hasAttribute('hidden');
-        if (disabled) {
-          btn.removeAttribute("hidden");
-        } else {
-          btn.setAttribute("hidden", "");
-        }
-    }
-  }
-  function foldWindow(tid){
-    let btn = document.getElementById(tid);
-    if (btn) {
-        let disabled = btn.hasAttribute('hidden');
-        if (disabled) {
-          btn.removeAttribute("hidden");
-        } else {
-          btn.setAttribute("hidden", "");
-        }
-    }
-  }
-  console.log("│  Folding  loaded  │");
-  console.log("│                   │");
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-document.getElementById('localtime').innerHTML = time
-  console.log("│    Time loaded    │");
-  console.log("│                   │");
-  console.log("│                   │");
-  console.log("│        End        │");
-  console.log("┕━━━━━━━━━━━━━━━━━━━┙");
+}
