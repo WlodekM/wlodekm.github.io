4c4f35e6b8f43601a627089baa36cbd59710372d
WlodekM <wlodzio.maly@gmail.com>
2023-04-05 16:26:48 +0300

    add stuff

--- files/testing/meowerhome/index.js
+++ files/testing/meowerhome/index.js
@@ -1,23 +1,33 @@
-async function getJSONData(url) {
-	const response = await fetch(url);
+async function getJSONData(url, params) {
+	const response = await fetch(url + new URLSearchParams(params));
 	const jsonData = await response.text();
 	return(Promise.resolve(jsonData));
   }
-  
+const urlSearchParams = new URLSearchParams(window.location.search);
+console.log(urlSearchParams);
+const params = Object.fromEntries(urlSearchParams.entries());
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let hpage = params.page; // "some_value"
+let is404 = params.error; // "some_value"
+console.log(`Page: ${hpage}`);
 var data_
 $(document).ready(function(){
-	$("body").prepend(`<div class=top><h1>Meower Posts</h1> <div id=controls><label for="badwords">Allow bad words:</label><input type="checkbox" id="badwords"><div id=spacer style="width:50px;"></div><button id="reload">Reload</button></div></div>`)
+	$("body").prepend(``)
 	reload()
 	$("#reload").click(function(){
-		$("#posts").empty();
+		reload()
+	});
+	$("#logo").click(function(){
 		reload()
 	});
 
 });
 function ghome(data) {
-		data_ = JSON.parse(data)
-		data_copy = JSON.parse(data)
-		data_["autoget"].forEach(element => {
+	data_ = JSON.parse(data)
+	data_copy = JSON.parse(data)
+	$("#posts").empty();
+	if(data_["error"] == false){
+	data_["autoget"].forEach(element => {
 		try {
 			if(element["unfiltered_p"] && (document.getElementById("badwords").checked)) {
 				$("#posts").append(`<div class="post"><span id=username>${element["u"]}</span><p>${element["unfiltered_p"]}</p></div>`)
@@ -34,9 +44,24 @@ function ghome(data) {
 			console.log(`ERROR ON:${element}`);
 		}
 		});
+	} else {
+		$("#posts").append(`<div class="post"><span id=username>There was an error while trying to get the page</span><p>Error: ${data_["type"]}</p><img src=https://raw.githubusercontent.com/meower-media-co/http-meower/main/imgs/${errors[data_["type"]]}.jpg></div>`)
+		//<img src=https://http.dog/${errors[data_["type"]]}.jpg>
+	}
 	
 }
 function reload() {
-	getJSONData('https://api.meower.org/home')
-	.then((data) => {ghome(data)})
+	if(hpage == null) {
+		if (is404) {
+			getJSONData('https://api.meower.org/404')
+			.then((data) => {ghome(data)})
+		} else {
+			getJSONData('https://api.meower.org/home')
+			.then((data) => {ghome(data)})
+		}
+	} else {
+		console.log(`Loading page ${hpage}`);
+		getJSONData(`https://api.meower.org/home?`,{"page":hpage})
+		.then((data) => {ghome(data)})
+	}
 }
