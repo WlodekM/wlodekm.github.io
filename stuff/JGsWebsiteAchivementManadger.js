6333b6d41ed82923daad176944875a495ef54c2d
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-05-23 15:46:08 +0300

    Update JGsWebsiteAchivementManadger.js

--- stuff/JGsWebsiteAchivementManadger.js
+++ stuff/JGsWebsiteAchivementManadger.js
@@ -2,14 +2,15 @@
 // javascript: req = new XMLHttpRequest(); req.open('GET', 'https://wlodekm.github.io/stuff/JGsWebsiteAchivementManadger.js'); req.onload = function() { eval(this.responseText); }; req.send();
 // Still dont know how to call this
 var wlodekmCheats = {}
+wlodekmCheats.page = document.getElementsByClassName("pagearea")[0]
 // Function for creating the menu element
 wlodekmCheats.createCheatMenuElement = (function(){
   let elem = document.createElement("div");
   elem.id = "wlodekmCheatMenu";
-  return(document.body.appendChild(elem))
+  return(wlodekmCheats.page.appendChild(elem))
 });
 wlodekmCheats.menu = wlodekmCheats.createCheatMenuElement()
-// Function for addin an element
+// Function for addin an element ( broken rn :( )
 wlodekmCheats.createElement = (function(type,id){
   let elem = document.createElement("div");
   elem.id = id;
