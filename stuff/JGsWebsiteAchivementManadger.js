858b09a0540a10d309e8c2c27e3eb61748dcf8b3
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-05-23 15:16:14 +0300

    Create JGsWebsiteAchivementManadger.js

---
+++ stuff/JGsWebsiteAchivementManadger.js
@@ -0,0 +1,2 @@
+// Work in progress
+// javascript: req = new XMLHttpRequest(); req.open('GET', 'https://wlodekm.github.io/stuff/JGsWebsiteAchivementManadger.js'); req.onload = function() { eval(this.responseText); }; req.send();
