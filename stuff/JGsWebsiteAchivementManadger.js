8968d6989f809cb81d0b356836585330b9f20c1e
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-05-23 15:37:45 +0300

    Update JGsWebsiteAchivementManadger.js

--- stuff/JGsWebsiteAchivementManadger.js
+++ stuff/JGsWebsiteAchivementManadger.js
@@ -1,2 +1,11 @@
 // Work in progress
 // javascript: req = new XMLHttpRequest(); req.open('GET', 'https://wlodekm.github.io/stuff/JGsWebsiteAchivementManadger.js'); req.onload = function() { eval(this.responseText); }; req.send();
+// Still dont know how to call this
+var wlodekmCheats = {}
+// Function for creating the menu element
+wlodekmCheats.createCheatMenuElement = (function(){
+  let elem = document.createElement("div");
+  elem.id = "wlodekmCheatMenu";
+  return(document.body.appendChild(elem))
+});
+wlodekmCheats.menu = wlodekmCheats.createCheatMenuElement()
